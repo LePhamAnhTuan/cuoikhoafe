6d4c5a10cbc7402da805cbb8e2e1765936aadd23
anhtuan <cntt.lpat.0211@gmail.com>
2023-07-24 18:37:40 +0700

    header & footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserTemplate from "./template/UserTemplate";
+import ListRoom from "./Component/ListRoom/ListRoom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>////</Routes>
+      <Routes>
+        <Route path="/" element={<UserTemplate />}>
+          <Route index element={<ListRoom />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/template/UserTemplate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Component/Header/Header";
+import Footer from "../Component/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+const UserTemplate = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default UserTemplate;
