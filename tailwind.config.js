41d1c1d9018abfb117730c7113108350868ef619
Tan Phu <phantanphu10@gmail.con>
2023-08-28 16:52:16 +0700

    update responsive info booked

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      tablet: "640px",
+      mobile: "640px",
       // => @media (min-width: 640px) { ... }
-
+      tablet: "768px",
+      // => @media (min-width: 768px) { ... }
       laptop: "1024px",
       // => @media (min-width: 1024px) { ... }
-
-      desktop: "1280px",
-      // => @media (min-width: 1280px) { ... }
+      desktop: "1240px",
+      // => @media (min-width: 1240px) { ... }
     },
   },
 
